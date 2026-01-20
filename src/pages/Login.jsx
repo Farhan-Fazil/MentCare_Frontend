58f116803e02715ffa6c745c8d8091aad06bd1fe
Farhan_Faz <113784191+Farhan-Fazil@users.noreply.github.com>
2026-01-20 09:54:19 +0530

    UX bug fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
 
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center '>
-      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#ffffff] text-sm shadow-lg'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-black text-sm shadow-lg'>
         <p className='text-2xl font-semibold'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</p>
         <p>Please {state === 'Sign Up' ? 'sign up' : 'log in'} to book appointment</p>
         {state === 'Sign Up'
